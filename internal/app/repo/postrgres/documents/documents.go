fa3ab59514450e453f7cc1a3b4be7cee03c62a92
ahtambaev.lev <ahtambaev.lev@wb.ru>
2024-04-04 21:44:02 +0300

    document dir deleted

--- internal/app/repo/postrgres/documents/documents.go
+++ internal/app/repo/postrgres/documents/documents.go
@@ -1,6 +1,11 @@
 package documents
 
-import "github.com/jmoiron/sqlx"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"github.com/jmoiron/sqlx"
+)
 
 // Repo инстанс репо для работы с загруженными документами
 type Repo struct {
@@ -12,4 +17,14 @@ func New(db *sqlx.DB) *Repo {
 	return &Repo{
 		db: db,
 	}
+}
+
+func (r *Repo) CheckExistence(ctx context.Context, vkID int) (bool, error) {
+	var exists bool
+	err := r.db.GetContext(ctx, &exists, "SELECT EXISTS (SELECT 1 FROM documents WHERE id = $1)", vkID)
+	if err != nil && err != sql.ErrNoRows {
+		return false, fmt.Errorf("[db.GetContext]: %w", err)
+	}
+
+	return exists, nil
 }
