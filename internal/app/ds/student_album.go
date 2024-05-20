56ae905bb4990a39b41902ae256f0c0cdf2261c6
Сергей <90382754+Nekrasov-Sergey@users.noreply.github.com>
2024-05-20 20:04:34 +0300

    Photo description album (#19)
    * small changes
    
    * add load archive with photo
    
    * add 6 states for load photo archive
    
    * add states for edit load photo archive
    
    * init search album
    
    * add 3 states search album
    
    * add state showListSearchAlbum
    
    * add state findYearLess2SearchAlbum
    
    * add 6 states for search album
    
    * add 2 states for search teacher's album
    
    * change load archive
    
    * add scrolling of teachers' names
    
    * added scrolling of found albums
    
    * add state SurnameTeacherSearchAlbumState
    
    * add GetSearchParams
    
    * add personal account photo
    
    ---------
    
    Co-authored-by: s.nekrasov <s.nekrasov@vk.team>

--- internal/app/ds/student_album.go
+++ internal/app/ds/student_album.go
@@ -5,6 +5,7 @@ type StudentAlbum struct {
 	Year         int    `db:"year"`
 	StudyProgram string `db:"study_program"`
 	Event        string `db:"event"`
+	Description  string `db:"description"`
 	URL          string `db:"url"`
 	VkID         int    `db:"vk_id"`
 }
--- internal/app/ds/teacher_album.go
+++ internal/app/ds/teacher_album.go
@@ -1,7 +1,9 @@
 package ds
 
 type TeacherAlbum struct {
-	ID   int    `db:"id"`
-	Name string `db:"name"`
-	URL  string `db:"url"`
+	ID          int    `db:"id"`
+	Name        string `db:"name"`
+	Description string `db:"description"`
+	URL         string `db:"url"`
+	VkID        int    `db:"vk_id"`
 }
